2f41660d76479b32dc926fbf0c8aa7a706ee6ce2
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-11-06 23:57:20 +0900

    Add shop page

---
+++ src/data/nfts.ts
@@ -0,0 +1,77 @@
+export const nfts = [
+  {
+    mint: "2H78TbeMR2KYeoYsNTjit3fKEV4k8T41Y6hBJmFxoTYZ",
+    owner: "EMvY6yGsb9s5TcBt9ppg1yJ6GJVnkTBeSv5JZDwvgptr",
+    name: "y00t #5289",
+    symbol: "Y00T",
+    description:
+      "y00ts is a generative art project of 15,000 NFTs. y00topia is a curated community of builders and creators. Each y00t was designed by De Labs in Los Angeles, CA.",
+    image: "https://metadata.y00ts.com/y/5288.png",
+    price: 2,
+    attributes: [
+      { trait_type: "Background", value: "Buttercream" },
+      { trait_type: "Fur", value: "Paradise Green" },
+      { trait_type: "Face", value: "Blasé" },
+      { trait_type: "Clothes", value: "Blue Sky Hoodie" },
+      { trait_type: "Eyewear", value: "Rembrandt Goggles" },
+      { trait_type: "Head", value: "Red Horns" },
+      { trait_type: "1/1", value: "None" },
+    ],
+    properties: {
+      files: [
+        { uri: "https://metadata.y00ts.com/y/5288.png", type: "image/png" },
+      ],
+      category: null,
+    },
+  },
+  {
+    mint: "9sYCmuxQk8QAoFrQoZCW8yRccW8z1nd7Tv36d7DemKmK",
+    owner: "EMvY6yGsb9s5TcBt9ppg1yJ6GJVnkTBeSv5JZDwvgptr",
+    name: "y00t #10314",
+    symbol: "Y00T",
+    description:
+      "y00ts is a generative art project of 15,000 NFTs. y00topia is a curated community of builders and creators. Each y00t was designed by De Labs in Los Angeles, CA.",
+    image: "https://metadata.y00ts.com/y/10313.png",
+    price: 3,
+    attributes: [
+      { trait_type: "Background", value: "Vanilla Ice" },
+      { trait_type: "Fur", value: "Eggnog" },
+      { trait_type: "Face", value: "Wholesome" },
+      { trait_type: "Clothes", value: "Sunday Shirt" },
+      { trait_type: "Eyewear", value: "Tints (blue)" },
+      { trait_type: "Head", value: "Backwards Snapback" },
+      { trait_type: "1/1", value: "None" },
+    ],
+    properties: {
+      files: [
+        { uri: "https://metadata.y00ts.com/y/10313.png", type: "image/png" },
+      ],
+      category: null,
+    },
+  },
+  {
+    mint: "EyzKyD6C58n6SZJTR4vVWneHKxGRixPDLgkvNZVjsxJx",
+    owner: "EMvY6yGsb9s5TcBt9ppg1yJ6GJVnkTBeSv5JZDwvgptr",
+    name: "y00t #1740",
+    symbol: "Y00T",
+    description:
+      "y00ts is a generative art project of 15,000 NFTs. y00topia is a curated community of builders and creators. Each y00t was designed by De Labs in Los Angeles, CA.",
+    image: "https://metadata.y00ts.com/y/1739.png",
+    price: 2,
+    attributes: [
+      { trait_type: "Background", value: "Solitary Star" },
+      { trait_type: "Fur", value: "Eggnog" },
+      { trait_type: "Face", value: "Blasé" },
+      { trait_type: "Clothes", value: "C.R.E.A.M. Hoodie" },
+      { trait_type: "Eyewear", value: "Abbey Roads" },
+      { trait_type: "Head", value: "Color Block Halo" },
+      { trait_type: "1/1", value: "None" },
+    ],
+    properties: {
+      files: [
+        { uri: "https://metadata.y00ts.com/y/1739.png", type: "image/png" },
+      ],
+      category: null,
+    },
+  },
+];
