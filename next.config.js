b73e15524c4669ae9c35b22d82ad43355a450a98
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-11-07 10:43:12 +0900

    Add NFT component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["metadata.y00ts.com"]
+  }
 }
 
 module.exports = nextConfig
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,3 +1,11 @@
+import NFTs from "../../components/NFTs";
+import SiteHeading from "../../components/SiteHeading";
+
 export default function ShopPage() {
-  
+  return (
+    <div className="relative flex flex-col items-stretch w-full gap-8 p-24 m-auto">
+      <SiteHeading>NFT Market</SiteHeading>
+      <NFTs />
+    </div>
+  );
 }
